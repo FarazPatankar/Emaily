b3356ed9b2fed2e4a51bc94f8faf60c8f0cc4a42
Faraz Patankar <farazpatankar@gmail.com>
2017-12-07 11:40:30 +0530

    setup google oauth with passport

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require('express');
+
+// We aren't exporting anything from the file. So it is fine to just require the entire file itself.
+require('./services/passport');
+
 const app = express();
+require('./routes/authRoutes')(app);
 
-app.get('/', (req, res) => {
-  res.send({ hi: 'there' });
-});
-
-
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 app.listen(PORT);
