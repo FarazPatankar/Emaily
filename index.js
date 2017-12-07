30ed01123c89c31b26b79b9e270b1477c49ef74f
Faraz Patankar <farazpatankar@gmail.com>
2017-12-07 12:03:24 +0530

    set up mongoose with mlab

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
 
 // We aren't exporting anything from the file. So it is fine to just require the entire file itself.
 require('./services/passport');
+
+mongoose.connect(keys.mongoURI);
 
 const app = express();
 require('./routes/authRoutes')(app);
